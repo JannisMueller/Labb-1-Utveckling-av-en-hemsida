077619b6043251ee769921c2befd6eb0377ff594
JannisMueller <jannis.mueller@hotmail.de>
2021-03-22 13:59:33 +0100

    adding validation to form

--- js/index.js
+++ js/index.js
@@ -3,47 +3,52 @@
 const elForm = document.querySelector('#formReg');
 const elName = document.querySelector('#textName');
 const elCheckTerms = document.querySelector('#terms');
+const elWorstShow = document.querySelector('#textWorstShow');
 const elTermsHint = document.querySelector('#termsHint');
 const elEmail = document.querySelector('#textEmail')
-const elFirst = document.querySelector('#textFirst');
-const elSecond = document.querySelector('#textSecond');
-const elThird = document.querySelector('#textThird');
-const elFourth = document.querySelector('#textFourth');
-const elFifth = document.querySelector('#textFifth');
 const elOutput = document.querySelector('#output');
+const elBtnShowWorstShow = document.querySelector('#btnShowWorstShow')
 
 
-function showListOfTvShows (event){
-
-    let name = elName.value;
-    let email = elEmail.value;
-
-    let firstPlace = elFirst.value;
-    let secondPlace = elSecond.value;
-    let thirdPlace = elThird.value;
-    let fourthPlace = elFourth.value;
-    let fifthPlace = elFifth.value;
-
-    elOutput.textContent =
-        + "1 " + firstPlace
-        + "2. " + secondPlace
-        + "3. " + thirdPlace
-        + "4. " + fourthPlace
-        + "5. " + fifthPlace
 
 
-    elForm.reset();
-    event.preventDefault();
-}
-
-
-function checkTerms(event) {                             // Declare function
+function checkTerms(event) {
     if (!elCheckTerms.checked) {                                // If checkbox ticked
         elTermsHint.innerHTML = 'You must agree to the terms.'; // Show message
         event.preventDefault();                              // Don't submit form
     }
+
+
 }
 
+
+function saveNameAndShow (event){
+    let name = elName.value;
+    let show = elWorstShow.value;
+    localStorage.setItem("name", name);
+    localStorage.setItem("show", show)
+
+    event.preventDefault();
+
+
+}
+
+function showNameAndShow(event){
+    let name = localStorage.getItem("name");
+    let show = localStorage.getItem("show");
+    elOutput.textContent = "" + show +  " is the worst you know, " + name + "!"
+
+    event.preventDefault();
+
+    elForm.reset();
+
+}
+
+
+elForm.addEventListener("submit", saveNameAndShow,false);
+elBtnShowWorstShow.addEventListener("click", showNameAndShow, false);
+
+
 elForm.addEventListener("submit", checkTerms,false);
-elForm.addEventListener("change",showListOfTvShows,false);
+
 
